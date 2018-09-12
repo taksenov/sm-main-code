34613ed72c4aa540c2205ae2ba6a54dac8bb797c
timofey.aksenov <taksenov@smedialink.com>
2018-09-12 11:08:24 +0500

    Refactoring Preloader

--- src/components/Preloader/index.tsx
+++ src/components/Preloader/index.tsx
@@ -1,11 +1,1 @@
-import * as React from 'react';
-
-const cx = require('classnames');
-import './styles.css';
-
-// прелоадер во всех подгурзках
-export class Preloader extends React.PureComponent {
-  public render() {
-    return <div className={cx('loader')} />;
-  }
-}
+export { default } from './Preloader';
